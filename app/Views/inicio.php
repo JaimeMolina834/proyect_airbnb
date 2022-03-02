41dba4f601e74ad5754ee080a8d4f1af00b2091b
JaimeMolina834 <jaimemolina2615@gmail.com>
2022-03-02 17:40:45 -0600

    update

--- app/Views/inicio.php
+++ app/Views/inicio.php
@@ -1,19 +1,6 @@
-<!doctype html>
-<html lang="es">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
-    
-    <!-- Style CSS -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>airbnb</title>
-  </head>
-  <body>
-
+  
     <!-- Carousel -->
 
 <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel">
