4dbea7428e5b5cb73a178a4869d790a4e6eaa70d
Diimaz <dimas98459@gmail.com>
2022-08-02 13:02:26 -0600

    update

---
+++ app/Entities/Servicio.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Entities;
+
+use CodeIgniter\Entity;
+
+class Servicio extends Entity{
+    /*Variables de tiempo en la entidad anfitrion*/
+    protected $dates = ['date_create','date_update'];
+}
