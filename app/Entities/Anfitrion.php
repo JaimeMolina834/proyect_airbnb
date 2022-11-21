cb0f8f49190628bda38d2fa17b3d1726b5998ae0
Diimaz <dimas98459@gmail.com>
2022-11-21 15:42:20 -0600

    update

--- app/Entities/Anfitrion.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace App\Entities;
-
-use CodeIgniter\Entity;
-
-class Anfitrion extends Entity{
-    /*Variables de tiempo en la entidad anfitrion*/
-    protected $dates = ['date_create','date_update'];
-}
