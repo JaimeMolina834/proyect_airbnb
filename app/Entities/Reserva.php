51ba97aed7affc3f76b90b6a9068595fa80d7e90
Diimaz <dimas98459@gmail.com>
2022-11-26 14:58:12 -0600

    update

--- app/Entities/Reserva.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Entities;
-
-use CodeIgniter\Entity;
-
-class Reserva extends Entity{
-    protected $dates = ['date_create','date_update'];
-}
--- app/Entities/Servicio.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace App\Entities;
-
-use CodeIgniter\Entity;
-
-class Servicio extends Entity{
-    protected $dates = ['date_create','date_update'];
-}
